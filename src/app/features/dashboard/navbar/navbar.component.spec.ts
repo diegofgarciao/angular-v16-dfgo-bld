7a8822f69f6bb5ddd6331ada2af6fd355be41a33
diegofgarciao <diegofgarciao@gmail.com>
2024-08-25 00:22:00 -0500

    refactor-para-responsive-movil

--- src/app/features/dashboard/navbar/navbar.component.spec.ts
+++ src/app/features/dashboard/navbar/navbar.component.spec.ts
@@ -1,21 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { NavbarComponent } from './navbar.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
-  let fixture: ComponentFixture<NavbarComponent>;
+  let router: Router;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [NavbarComponent]
     });
-    fixture = TestBed.createComponent(NavbarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    component = TestBed.createComponent(NavbarComponent).componentInstance;
+    router = TestBed.inject(Router);
   });
 
-  it('should create', () => {
+  it('should create the navbar component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should navigate to login on logout', () => {
+    const navigateSpy = spyOn(router, 'navigate');
+    component.logout();
+    expect(navigateSpy).toHaveBeenCalledWith(['/login']);
+  });
+
 });
