a743a81e63c819fa39ce60ad4ff22023d3e90a42
diegofgarciao <diegofgarciao@gmail.com>
2024-08-26 19:18:47 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Servir los archivos estáticos
-app.use(express.static(__dirname + '/dist/my-app'));
+app.use(express.static(__dirname + '/dist/angular-v16-bold-project-dfgo'));
 
 // Ruta de fallback para la SPA
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/my-app/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/angular-v16-bold-project-dfgo/index.html'));
 });
 
 // Escucha en el puerto especificado por Heroku
